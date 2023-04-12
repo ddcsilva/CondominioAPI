2206c149c17909b1220edc0dc5af2ad2480a4482
danilo.silva <danilo.silva@msn.com>
2023-04-11 22:24:25 -0300

    Adicionado repositório e interface do repositório para a entidade Condominio

---
+++ CondominioAPI/CondominioAPI.Domain/Repositories/CondominioRepository.cs
@@ -0,0 +1,46 @@
+﻿using CondominioAPI.Domain.Entities;
+
+namespace CondominioAPI.Domain.Repositories
+{
+    public class CondominioRepository : ICondominioRepository
+    {
+        private readonly List<Condominio> _condominios = new List<Condominio>();
+
+        public async Task<IEnumerable<Condominio>> GetAllAsync()
+        {
+            return await Task.FromResult(_condominios.ToList());
+        }
+
+        public async Task<Condominio?> GetByIdAsync(Guid id)
+        {
+            var condominio = _condominios.FirstOrDefault(c => c.Id == id);
+            return await Task.FromResult(condominio);
+        }
+
+        public async Task<Condominio> AddAsync(Condominio condominio)
+        {
+            _condominios.Add(condominio);
+            return await Task.FromResult(condominio);
+        }
+
+        public async Task UpdateAsync(Condominio condominioToUpdate)
+        {
+            var index = _condominios.FindIndex(c => c.Id == condominioToUpdate.Id);
+            if (index >= 0)
+            {
+                _condominios[index] = condominioToUpdate;
+            }
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var condominio = _condominios.FirstOrDefault(c => c.Id == id);
+            if (condominio != null)
+            {
+                _condominios.Remove(condominio);
+            }
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ CondominioAPI/CondominioAPI.Domain/Repositories/ICondominioRepository.cs
@@ -0,0 +1,13 @@
+﻿using CondominioAPI.Domain.Entities;
+
+namespace CondominioAPI.Domain.Repositories
+{
+    public interface ICondominioRepository
+    {
+        Task<IEnumerable<Condominio>> GetAllAsync();
+        Task<Condominio?> GetByIdAsync(Guid id);
+        Task<Condominio> AddAsync(Condominio condominio);
+        Task UpdateAsync(Condominio condominio);
+        Task DeleteAsync(Guid id);
+    }
+}
