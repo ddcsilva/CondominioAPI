e9c0961c8be42fa0cf5a70ec6af299a1383d2ae2
danilo.silva <danilo.silva@msn.com>
2023-04-11 22:08:52 -0300

    Adicionado entidade Condominio no projeto Domain

---
+++ CondominioAPI/CondominioAPI.Domain/Entities/Condominio.cs
@@ -0,0 +1,13 @@
+﻿namespace CondominioAPI.Domain.Entities
+{
+    public class Condominio
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Endereco { get; set; }
+        public int NumeroUnidades { get; set; }
+        public int? NumeroBlocos { get; set; }
+        public DateTime DataFundacao { get; set; }
+    }
+}
