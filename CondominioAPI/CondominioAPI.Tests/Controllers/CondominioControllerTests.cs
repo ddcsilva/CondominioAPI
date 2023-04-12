b89e41e9423b4f968c9454a77fdb1520e1d57c8e
danilo.silva <danilo.silva@msn.com>
2023-04-12 17:21:54 -0300

    Implementação de testes unitários e de integração para CondominioService e CondominioController

---
+++ CondominioAPI/CondominioAPI.Tests/Controllers/CondominioControllerTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using CondominioAPI.Domain.Entities;
+
+namespace CondominioAPI.Tests.Controllers
+{
+    public class CondominioControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly HttpClient _client;
+
+        public CondominioControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ObterCondominios_RetornaStatusCodeDeSucesso()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/condominio");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task ObterCondominioPorId_RetornaStatusCodeDeSucesso()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var response = await _client.GetAsync($"/api/condominio/{id}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task AdicionarCondominio_RetornaStatusCodeDeCriado()
+        {
+            // Arrange
+            var condominio = new Condominio
+            {
+                Nome = "Condominio A",
+                CNPJ = "00.000.000/0000-00",
+                Endereco = "Rua A, 123",
+                NumeroUnidades = 10,
+                NumeroBlocos = 2,
+                DataFundacao = DateTime.Now.AddYears(-5)
+            };
+
+            // Act
+            var response = await _client.PostAsJsonAsync("/api/condominio", condominio);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task AtualizarCondominio_RetornaStatusCodeDeSucesso()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var condominio = new Condominio
+            {
+                Nome = "Condominio A",
+                CNPJ = "00.000.000/0000-00",
+                Endereco = "Rua A, 123",
+                NumeroUnidades = 10,
+                NumeroBlocos = 2,
+                DataFundacao = DateTime.Now.AddYears(-5)
+            };
+
+            // Act
+            var response = await _client.PutAsJsonAsync($"/api/condominio/{id}", condominio);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeletarCondominio_RetornaStatusCodeDeSemConteudo()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var response = await _client.DeleteAsync($"/api/condominio/{id}");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ CondominioAPI/CondominioAPI.Tests/Services/CondominioServiceTests.cs
@@ -0,0 +1,122 @@
+﻿using CondominioAPI.Domain.Entities;
+using CondominioAPI.Domain.Repositories;
+using CondominioAPI.Domain.Services;
+using Moq;
+
+namespace CondominioAPI.Tests.Services
+{
+    public class CondominioServiceTests
+    {
+        private readonly Mock<ICondominioRepository> _mockRepository;
+        private readonly CondominioService _service;
+
+        public CondominioServiceTests()
+        {
+            _mockRepository = new Mock<ICondominioRepository>();
+            _service = new CondominioService(_mockRepository.Object);
+        }
+
+        [Fact]
+        public async Task ObterTodosCondominiosAssincrono_RetornaTodosOsCondominios()
+        {
+            // Arrange
+            var condominios = new List<Condominio>
+            {
+                new Condominio
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = "Condominio A",
+                    CNPJ = "00.000.000/0000-00",
+                    Endereco = "Rua A, 123",
+                    NumeroUnidades = 10,
+                    NumeroBlocos = 2,
+                    DataFundacao = DateTime.Now.AddYears(-5)
+                },
+                new Condominio
+                {
+                    Id = Guid.NewGuid(),
+                    Nome = "Condominio B",
+                    CNPJ = "11.111.111/1111-11",
+                    Endereco = "Rua B, 456",
+                    NumeroUnidades = 20,
+                    NumeroBlocos = 4,
+                    DataFundacao = DateTime.Now.AddYears(-10)
+                }
+            };
+
+            _mockRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(condominios);
+
+            // Act
+            var result = await _service.GetAllAsync();
+
+            // Assert
+            Assert.Equal(condominios, result);
+            _mockRepository.Verify(repo => repo.GetAllAsync(), Times.Once());
+        }
+
+        [Fact]
+        public async Task AdicionarCondominioAssincrono_RetornaCondominioAdicionado()
+        {
+            // Arrange
+            var condominio = new Condominio
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Condominio A",
+                CNPJ = "00.000.000/0000-00",
+                Endereco = "Rua A, 123",
+                NumeroUnidades = 10,
+                NumeroBlocos = 2,
+                DataFundacao = DateTime.Now.AddYears(-5)
+            };
+
+            _mockRepository.Setup(repo => repo.AddAsync(condominio)).ReturnsAsync(condominio);
+
+            // Act
+            var result = await _service.AddAsync(condominio);
+
+            // Assert
+            Assert.Equal(condominio, result);
+            _mockRepository.Verify(repo => repo.AddAsync(condominio), Times.Once());
+        }
+
+
+        [Fact]
+        public async Task AtualizarCondominioAssincrono_RetornaCondominioAtualizado()
+        {
+            // Arrange
+            var condominio = new Condominio
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Condominio A",
+                CNPJ = "00.000.000/0000-00",
+                Endereco = "Rua A, 123",
+                NumeroUnidades = 10,
+                NumeroBlocos = 2,
+                DataFundacao = DateTime.Now.AddYears(-5)
+            };
+
+            _mockRepository.Setup(repo => repo.UpdateAsync(condominio)).Returns(Task.CompletedTask);
+
+            // Act
+            await _service.UpdateAsync(condominio);
+
+            // Assert
+            _mockRepository.Verify(repo => repo.UpdateAsync(condominio), Times.Once());
+        }
+
+        [Fact]
+        public async Task ExcluirCondominioAssincrono_RetornaCondominioExcluido()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _mockRepository.Setup(repo => repo.DeleteAsync(id)).Returns(Task.CompletedTask);
+
+            // Act
+            await _service.DeleteAsync(id);
+
+            // Assert
+            _mockRepository.Verify(repo => repo.DeleteAsync(id), Times.Once());
+        }
+
+    }
+}
