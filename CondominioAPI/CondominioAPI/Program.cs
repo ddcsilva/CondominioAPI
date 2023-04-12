2b0771173b69e90a3fd3d11f5adb50ba204e5814
danilo.silva <danilo.silva@msn.com>
2023-04-11 22:29:04 -0300

    Configuração da injeção de dependências para o repositório de Condominio

--- CondominioAPI/CondominioAPI/Program.cs
+++ CondominioAPI/CondominioAPI/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<ICondominioRepository, CondominioRepository>();
 
 var app = builder.Build();
